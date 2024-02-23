b2d07c2629481ab875c7e43d90de4f2053c43607
Swapnil-Ahirrao997539 <159124232+Swapnil-Ahirrao997539@users.noreply.github.com>
2024-02-23 13:37:42 +0530

    theme issue resolved for material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,52 @@
 import { Component } from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import {NestedTreeControl} from '@angular/cdk/tree';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
+      },
+      {
+        name: 'Orange',
+        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
+      },
+    ],
+  },
+];
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +56,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Ang-scss';
   hide = true;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource1 = new MatTreeNestedDataSource<FoodNode>();
+
+  constructor() {
+    this.dataSource1.data = TREE_DATA;
+  }
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+
 
 }
